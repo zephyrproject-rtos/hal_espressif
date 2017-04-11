338b8dbabc18baec1ed0f7d518d7042f5f04f31c
Ivan Grokhotkov <ivan@espressif.com>
2017-04-11 15:44:43 +0800

    soc: add source code of rtc_clk, rtc_pm

--- components/soc/esp32/include/esp_brownout.h
+++ components/soc/esp32/include/esp_brownout.h
@@ -1,4 +1,4 @@
-// Copyright 2010-2016 Espressif Systems (Shanghai) PTE LTD
+// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,23 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef _SOC_BB_REG_H_
-#define _SOC_BB_REG_H_
 
-/* Some of the baseband control registers.
- * PU/PD fields defined here are used in sleep related functions.
- */
+#ifndef __ESP_BROWNOUT_H
+#define __ESP_BROWNOUT_H
 
-#define BBPD_CTRL (DR_REG_BB_BASE + 0x0054)
-#define BB_FFT_FORCE_PU (BIT(3))
-#define BB_FFT_FORCE_PU_S 3
-#define BB_FFT_FORCE_PD (BIT(2))
-#define BB_FFT_FORCE_PD_S 2
-#define BB_DC_EST_FORCE_PU (BIT(1))
-#define BB_DC_EST_FORCE_PU_S 1
-#define BB_DC_EST_FORCE_PD (BIT(0))
-#define BB_DC_EST_FORCE_PD_S 0
+void esp_brownout_init();
 
-
-#endif /* _SOC_BB_REG_H_ */
-
+#endif
