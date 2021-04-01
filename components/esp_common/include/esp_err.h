74dfb876bd852ac30a2f78ae3d7789235b40d269
Sylvio Alves <sylvio.alves@espressif.com>
2021-03-31 23:16:53 -0300

    log: remove build warnings
    BT support add a few warnings and logging errors
    
    Signed-off-by: Sylvio Alves <sylvio.alves@espressif.com>

--- components/esp_common/include/esp_err.h
+++ components/esp_common/include/esp_err.h
@@ -74,8 +74,13 @@ const char *esp_err_to_name(esp_err_t code);
   */
 const char *esp_err_to_name_r(esp_err_t code, char *buf, size_t buflen);
 
+#if __ZEPHYR__
+/** @cond */
+void _esp_error_check_failed(esp_err_t rc, const char *file, int line, const char *function, const char *expression);
+#else
 /** @cond */
 void _esp_error_check_failed(esp_err_t rc, const char *file, int line, const char *function, const char *expression) __attribute__((noreturn));
+#endif
 
 /** @cond */
 void _esp_error_check_failed_without_abort(esp_err_t rc, const char *file, int line, const char *function, const char *expression);
