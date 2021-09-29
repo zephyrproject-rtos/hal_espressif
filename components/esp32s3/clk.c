917560cace84e4c2b32b13b12e17409ad4a6df01
Sylvio Alves <sylvio.alves@espressif.com>
2021-09-29 00:48:49 -0300

    clock: add soc specific clock functions
    This enabled proper soc clock settings defintions.
    
    Signed-off-by: Sylvio Alves <sylvio.alves@espressif.com>

--- components/esp32s3/clk.c
+++ components/esp32s3/clk.c
@@ -19,24 +19,22 @@
 #include "esp32s3/clk.h"
 #include "soc/rtc.h"
 
-#define MHZ (1000000)
-
 // g_ticks_us defined in ROMs for PRO and APP CPU
 extern uint32_t g_ticks_per_us_pro;
 
 int IRAM_ATTR esp_clk_cpu_freq(void)
 {
-    return g_ticks_per_us_pro * MHZ;
+    return MHZ(g_ticks_per_us_pro);
 }
 
 int IRAM_ATTR esp_clk_apb_freq(void)
 {
-    return MIN(g_ticks_per_us_pro, 80) * MHZ;
+    return MHZ(MIN(g_ticks_per_us_pro, 80));
 }
 
 int IRAM_ATTR esp_clk_xtal_freq(void)
 {
-    return rtc_clk_xtal_freq_get() * MHZ;
+    return MHZ(rtc_clk_xtal_freq_get());
 }
 
 void IRAM_ATTR ets_update_cpu_frequency(uint32_t ticks_per_us)
