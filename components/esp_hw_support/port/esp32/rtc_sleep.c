4fc14f675143d71496f54558d8539b66c7e3283e
Glauber Maroto Ferreira <glauber.ferreira@espressif.com>
2022-08-22 19:44:25 -0300

    hal: pm: esp32: tweak components
    - Tweaks/updates esp32 components related to
    deep sleep mode building
    - Add system clk cycles definition to stubs.
    
    Signed-off-by: Glauber Maroto Ferreira <glauber.ferreira@espressif.com>

--- components/esp_hw_support/port/esp32/rtc_sleep.c
+++ components/esp_hw_support/port/esp32/rtc_sleep.c
@@ -264,7 +264,7 @@ uint32_t rtc_deep_sleep_start(uint32_t wakeup_opt, uint32_t reject_opt)
     const unsigned CRC_LEN = 0x7ff;
     const unsigned RTC_MEM_PID = 1;
 
-    asm volatile(
+    __asm__ volatile(
                  "movi a2, 0\n" // trigger a stack spill on working register if needed
 
                  /* Start CRC calculation */
