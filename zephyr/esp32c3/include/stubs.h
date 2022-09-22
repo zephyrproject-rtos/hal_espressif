d1afae79e0a9e88a8c0711e126c93b3018c79208
Glauber Maroto Ferreira <glauber.ferreira@espressif.com>
2022-09-22 12:21:12 -0300

    hal: pm: esp32c3: light_sleep: CMakeLists & stubs update
    - Add system clock cycle definition to stub file.
    - Update CMakeLists.
    
    Signed-off-by: Glauber Maroto Ferreira <glauber.ferreira@espressif.com>

--- zephyr/esp32c3/include/stubs.h
+++ zephyr/esp32c3/include/stubs.h
@@ -16,4 +16,11 @@
 
 #define ESP_SOC_DEFAULT_CPU_FREQ_MHZ (CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC / 1000000)
 
+#if !defined(CONFIG_ESP_CONSOLE_UART_BAUDRATE) && defined(CONFIG_ESP_CONSOLE_UART)
+#define CONFIG_ESP_CONSOLE_UART_BAUDRATE                \
+        ((CONFIG_ESP_CONSOLE_UART_NUM)?                 \
+         DT_PROP(DT_NODELABEL(uart1), current_speed) :  \
+         DT_PROP(DT_NODELABEL(uart0), current_speed))
+#endif
+
 #endif /* _STUBS_H_ */
