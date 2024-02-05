f068cbf9f571369b1a7f4a6c3673f87ef6e522f6
Sylvio Alves <sylvio.alves@espressif.com>
2024-02-05 08:39:21 -0300

    esp_shared: delete unused file and use port folder
    This commit deletes duplicated or unused esp_shared content
    and also move other files as such as pinctrl to port folder.
    
    Signed-off-by: Sylvio Alves <sylvio.alves@espressif.com>

--- zephyr/esp_shared/include/boot/bootloader_init_common.h
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-#pragma once
-
-void bootloader_print_flash_info(const esp_image_header_t *bootloader_hdr);
