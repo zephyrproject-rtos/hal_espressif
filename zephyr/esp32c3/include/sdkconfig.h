a1a92068be550132fd9eed81c44ff290ca3db9e3
Marcio Ribeiro <marcio.ribeiro@espressif.com>
2024-07-24 15:01:17 -0300

    improvement: sdkconfig removal
    Moves SoCs sdkconfig.h contents to a suitable Kconfig file
    
    Signed-off-by: Marcio Ribeiro <marcio.ribeiro@espressif.com>

--- zephyr/esp32c3/include/sdkconfig.h
+++ zephyr/esp32c3/include/sdkconfig.h
@@ -3,26 +3,3 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
-#ifdef CONFIG_MCUBOOT
-
-#define CONFIG_SPI_FLASH_ROM_DRIVER_PATCH 1
-#define CONFIG_EFUSE_VIRTUAL_OFFSET 0x250000
-#define CONFIG_EFUSE_VIRTUAL_SIZE 0x2000
-#define CONFIG_EFUSE_MAX_BLK_LEN 256
-
-#endif /* CONFIG_MCUBOOT*/
-
-#define CONFIG_IDF_FIRMWARE_CHIP_ID 0x0005
-#define CONFIG_LOG_TIMESTAMP_SOURCE_RTOS 1
-#define CONFIG_BOOTLOADER_LOG_LEVEL 3
-
-#define CONFIG_BT_CTRL_ESP32C3 1
-#define CONFIG_BT_SOC_SUPPORT_5_0 1
-#define BTDM_CTRL_HCI_MODE_VHCI 1
-
-#undef asm
-#define asm __asm__
-
-#undef typeof
-#define typeof __typeof__
--- zephyr/esp32c6/include/sdkconfig.h
+++ zephyr/esp32c6/include/sdkconfig.h
@@ -3,15 +3,3 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
-#define CONFIG_IDF_FIRMWARE_CHIP_ID 0x000D
-#define CONFIG_LOG_TIMESTAMP_SOURCE_RTOS 1
-#define CONFIG_BOOTLOADER_LOG_LEVEL 3
-
-#define CONFIG_IDF_TARGET_ESP32C6 1
-
-#undef asm
-#define asm __asm__
-
-#undef typeof
-#define typeof __typeof__
