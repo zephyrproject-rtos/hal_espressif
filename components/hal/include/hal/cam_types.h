69098633c704a60b5cfd1522c9904a44163a1238
Lucas Tamborrino <lucas.tamborrino@espressif.com>
2024-08-14 16:20:19 -0300

    hal: add CAM HAL files
    Add CAM files to provide a proper HAL API
    for Zephyr's camera driver.
    
    Signed-off-by: Lucas Tamborrino <lucas.tamborrino@espressif.com>

---
+++ components/hal/include/hal/cam_types.h
@@ -0,0 +1,35 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include "soc/soc_caps.h"
+#include "soc/clk_tree_defs.h"
+#include "hal/cam_ctlr_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if SOC_LCDCAM_CAM_DATA_WIDTH_MAX
+#define CAP_DVP_PERIPH_NUM      SOC_LCDCAM_CAM_PERIPH_NUM   /*!< DVP port number */
+#define CAM_DVP_DATA_SIG_NUM    SOC_LCDCAM_CAM_DATA_WIDTH_MAX   /*!< DVP data bus width of CAM */
+#else
+#define CAP_DVP_PERIPH_NUM      0                           /*!< Default value */
+#define CAM_DVP_DATA_SIG_NUM    0                           /*!< Default value */
+#endif
+
+#if SOC_LCDCAM_CAM_SUPPORTED
+typedef soc_periph_cam_clk_src_t cam_clock_source_t;        /*!< Clock source type of CAM */
+#elif SOC_LCDCAM_SUPPORTED
+typedef soc_periph_lcd_clk_src_t cam_clock_source_t;
+#else
+typedef int cam_clock_source_t;                             /*!< Default type */
+#endif
+
+#ifdef __cplusplus
+}
+#endif
