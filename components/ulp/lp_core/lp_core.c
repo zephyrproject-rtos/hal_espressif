275cb244106eda2913836eb8f1bce0c8955249e4
Lucas Tamborrino <lucas.tamborrino@espressif.com>
2025-02-06 15:01:12 -0300

    ulp: get coprocessor reserved memory from dts
    Get ULP_COPROC_RESERVE_MEM value from dts.
    
    Signed-off-by: Lucas Tamborrino <lucas.tamborrino@espressif.com>

--- components/ulp/lp_core/lp_core.c
+++ components/ulp/lp_core/lp_core.c
@@ -19,7 +19,7 @@
 #include "hal/lp_core_ll.h"
 #include <zephyr/devicetree.h>
 
-#define ULP_COPROC_RESERVE_MEM (0x4000)
+#define ULP_COPROC_RESERVE_MEM DT_REG_SIZE(DT_NODELABEL(sramlp))
 
 #if CONFIG_IDF_TARGET_ESP32P4
 #include "esp32p4/rom/rtc.h"
